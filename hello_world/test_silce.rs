1661adebcc8469d20e7ab44eddb5d84bf22ba1b8
ZhaolongLi <0x4f4f4f4f@gmail.com>
2022-06-06 22:16:46 +0800

    guess game

---
+++ hello_world/test_silce.rs
@@ -0,0 +1,51 @@
+
+fn main() {
+    // let mut s = String::from("hello world");
+
+    // let word = first_word(&s); // word will get the value 5
+    // println!(": {}", word);
+    // s.clear(); // this empties the String, making it equal to ""
+    // println!("the first word is: {}", word);
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+}
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     // 遍历bytes的迭代器，enumerate做一个包装，使得返回值为索引和引用
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
